3f8e207b1ee0725c8c1719e22f6f2040614e5033
yzm <379852058@qq.com>
2023-03-25 16:23:58 +0800

    second a blog test program

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,8 @@
+const moment = require('moment');
+moment.locale("zh-cn")
+
 module.exports = {
-    // plugins: [
-    //     ["vuepress-plugin-auto-sidebar", {
-    //       // options
-    //     }]
-    //   ],
+    base: "/docs/",
     title: "凡事发生必有利于我",
     description: "凡事发生必有利于我的随记",
     head: [
@@ -11,6 +10,21 @@ module.exports = {
         ['meta',{name: 'author', content: 'Salvatore'}],
         ['meta',{name: 'keywords', content: '算法刷题记录，编程语言技术文档，面试八股记录，凡事发生必有利于我'}],
     ],
+    plugins: [
+        [
+          '@vuepress/last-updated',
+          {
+            transformer: (timestamp) => {
+              // 不要忘了安装 moment
+              const moment = require('moment')
+              return moment(timestamp).format("LLLL")
+            }
+          }
+        ]
+        // ["vuepress-plugin-auto-sidebar", {
+        //     // options
+        // }]
+      ],
     themeConfig: {
       lastUpdated: 'Last Updated',
       logo: '/assets/img/logo.png',
